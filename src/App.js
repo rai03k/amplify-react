b2f374c913263ff9298e92d8e602547e17826bb1
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-10-25 12:28:39 +0900

    shinya test app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shinya React App
         </a>
       </header>
     </div>
